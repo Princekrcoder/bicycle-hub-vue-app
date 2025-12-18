1001de9c4446b38a46beccc738f9918d828d4dc1
Princekrcoder <princekrcoder@gmail.com>
2025-12-18 20:45:40 +0530

    Auto commit: 18-12-2025 20:45:40.27

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,9 +1,13 @@
 <script setup>
+import { ref } from "vue";
 
 </script>
 
 <template>
-  <div></div>
+  <div>
+    <h1>Hello, Bicycle Hub!</h1>
+    <p>This is the HelloWorld component.</p>
+  </div>
 </template>
 
 
